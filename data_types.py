40e1cec7d111e703bfb07ffe27d6c421b9c38fea
Alex <Litvinchukas@ukr.net>
2022-10-23 13:32:15 +0300

    my 2 homework

--- data_types.py
+++ data_types.py
@@ -20,3 +20,33 @@ print('Minimum salary', '-',min_salary, "usd." )
 print('Company budget per month', '-',salary_budge, "usd.")
 print('Employee insurance', '-',insurance_empl, "usd", ' per year.')
 print('Total insurance budget - ', qty_employees*insurance_empl, "usd per year.")
+###########################################################################
+avg_salary_: int = 2040
+qty_employees_: int = 18
+max_salary_: int = 3500
+min_salary_: int = 1500
+insurance_empl_: int = 480
+salary_budge_: int = avg_salary * qty_employees
+
+company_name_:str = ('First IT Solution')
+country_of_registration_:str = ('Ukraine')
+location_city_:str = ('Kyiv')
+director_fr_name_:str = ('Robert')
+director_ls_name_:str = ('Li')
+
+print(type(avg_salary_))
+print(type(company_name_))
+print(type(qty_employees_))
+print(type(country_of_registration_))
+
+print('AVG salary - ', int(avg_salary_))
+print('QTY employees - ', int(qty_employees_))
+print('Max salary - ', int(max_salary_))
+print('Min salary - ', int(min_salary_))
+print('Insurance empl - ', int(insurance_empl_))
+print('Salary_budge - ', int(salary_budge_))
+
+print('Company name - ', str(company_name_))
+print('Country of registration - ', str(country_of_registration_))
+print('Location city - ', str(location_city_))
+print('Director name - ', str(director_fr_name_), str(director_ls_name_))
