25000090a32279e0a0106e7aaf4816115d0673ac
Alex <Litvinchukas@ukr.net>
2022-10-25 19:25:09 +0300

    my 3 homework (bool logic)

---
+++ boll_type.py
@@ -0,0 +1,45 @@
+les_1 = 3!=5
+print ('Exercise 1 - ', les_1)
+###############
+
+les_21 = 5==5
+print ('Exercise 2.1 - ',  les_21)
+les_22 = 5 >= 5
+print ('Exercise 2.2 - ',  les_22)
+les_23 = 5 <= 5
+print ('Exercise 2.3 - ',  les_23)
+################
+
+Les_31 = True and True or False
+print ('Exercise 3.1 - ', Les_31)
+
+Les_32 = True or True and False
+print ('Exercise 3.2 - ', Les_32)
+
+Les_33 = True or True or False
+print ('Exercise 3.3 - ', Les_33)
+
+Les_34 = True and True is not False
+print ('Exercise 3.4 - ', Les_34)
+
+Les_35 = True or True is not False
+print ('Exercise 3.5 - ', Les_35)
+##############################
+
+les_41 = (bool(None)) == 7
+print ('Exercise 4.1 - ', les_41)
+
+les_42 = (bool(' ')) == 10-1
+print ('Exercise 4.2 - ', les_42)
+
+les_43 = (bool(True or False)) == Les_35
+print ('Exercise 4.3 - ', les_43)
+
+les_441 = type(None)
+print (les_441)
+
+les_451 = id(None)
+print (les_451)
+
+les_44 = (bool(type(None))) == id(None)
+print ('Exercise 4.4 - ', les_44)
