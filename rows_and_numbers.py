21d1cace0d0df923d9a93e18c1738540747a6b78
Alex <Litvinchukas@ukr.net>
2022-10-26 20:49:19 +0300

    my 3 homework (str)

---
+++ rows_and_numbers.py
@@ -0,0 +1,9 @@
+user = input('What is your name?')
+hi_user = f'Hello {user}'
+print(hi_user)
+
+weight = input('What is your weight?')
+weight_user = f'Weight -  {weight}'
+print(weight_user)
+weight_user_ = int(weight_use)
+print (weight_user_)
