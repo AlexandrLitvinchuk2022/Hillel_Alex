46cb439fa7094cdb111d512fcca2a439a4fb4216
Alex <Litvinchukas@ukr.net>
2022-11-06 15:12:19 +0200

    my 7 homework

---
+++ test1.py
@@ -0,0 +1,41 @@
+#inputed = input("Enter word: ")
+#space_ind = ''
+#indexes =[]
+#for  index, element in enumerate(inputed):
+#    #print(f'{index=} | {element=}')
+#    if element == " ":
+#        indexes.append(index)
+
+#print(indexes)
+
+##############################################3
+inputed = input("Enter word: ")
+uppere_case = ''
+space_ind = ''
+gl_wodr = ''
+digit_counter = 0
+indexes =[]
+for element in inputed:
+    if element.isdigit():
+        digit_counter +=1
+
+        if digit_counter ==3:
+            break
+
+    else:
+        if element.isupper():
+            uppere_case += element
+
+        if element in 'IOEAUYioeauy':
+            gl_wodr += element
+
+for  index, element in enumerate(inputed):
+    #print(f'{index=} | {element=}')
+    if element == " ":
+        indexes.append(index)
+
+
+print(uppere_case)
+print(space_ind)
+print(gl_wodr)
+print(indexes)
