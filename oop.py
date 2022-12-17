f519aae2a8b892691db8cfd68685ba111cebccf9
Alex <Litvinchukas@ukr.net>
2022-12-17 18:25:53 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ oop.py
@@ -0,0 +1,52 @@
+# class Animal:
+#     population: int = 30  # attribute of the class
+#
+# animal = Animal()
+# print(f"{dir(animal)=}")
+# print(f"{animal.population=}"
+
+#######################################
+
+class Mage:
+    population: int = 30
+    power: int = 10
+    superpower: int = 3
+
+mage = Mage()
+#print(f"{dir(mage)=}")
+print(f"{mage.population=}")
+print(f"{mage.power=}")
+print(f"{mage.superpower=}")
+
+class Archer:
+    population: int = 20
+    power: int = 10
+    superpower: int = 5
+
+archer = Archer()
+#print(f"{dir(archer)=}")
+print(f"{archer.population=}")
+print(f"{archer.power=}")
+print(f"{archer.superpower=}")
+
+class Knight:
+    population: int = 14
+    power: int = 10
+    superpower: int = 8
+
+knight = Knight()
+#print(f"{dir(knight)=}")
+print(f"{knight.population=}")
+print(f"{knight.power=}")
+print(f"{knight.superpower=}")
+
+class Orc:
+    population: int = 40
+    power: int = 10
+    superpower: int = 5
+
+orc = Orc()
+#print(f"{dir(knight)=}")
+print(f"{orc.population=}")
+print(f"{orc.power=}")
+print(f"{orc.superpower=}")
